553277fa649684c7ef510b441e4da8fc2bd440be
ReubenScott <ReubenLScott@Hotmail.com>
2014-11-21 10:42:59 +0800

    db struct

---
+++ install/struct.sql
@@ -0,0 +1,30 @@
+
+-- 站点配置
+SELECT * FROM qinggan_site;
+
+-- 系统用户
+SELECT * FROM qinggan_adm;
+
+-- 内容管理
+SELECT * FROM qinggan_project;
+
+-- 详细内容
+SELECT * FROM qinggan_list;
+
+-- 内容 分类
+SELECT * FROM qinggan_cate;
+
+-- 模块
+select * from qinggan_module ;
+
+-- 记录上传图片
+SELECT * FROM qinggan_res;
+
+-- 产品 关联 图片
+SELECT * FROM qinggan_list_24;
+
+
+--  联系我们 
+SELECT * FROM qinggan_ext;
+SELECT * FROM qinggan_extc;
+
